0eacde339f11f261cc75108309517897e6b9f01f
piaskowskirodrigo <61170540+piaskowskirodrigo@users.noreply.github.com>
2024-03-19 17:28:51 -0300

    Projeto final Poo

---
+++ JogoPalavrasEmbaralhadas/src/jogo/FabricaMecanicaDoJogo.java
@@ -0,0 +1,16 @@
+package jogo;
+
+import java.io.IOException;
+
+public class FabricaMecanicaDoJogo {
+	
+	public MecanicaDoJogo getMecanicaEasy () throws IOException
+	{
+		return new MecanicaEasy(); 
+	}
+	
+	public MecanicaDoJogo getMecanicaHard () throws IOException
+	{
+		return new MecanicaHard(); 
+	}
+}
